cf507f7849a35653ff2c75dafd806047c09cdde0
Sofia  Rocha <sofia.cavalcanti@ccc.ufcg.edu.br>
2020-10-15 21:49:38 -0300

    Add files via upload

---
+++ HelloWorld/hello.java
@@ -0,0 +1,11 @@
+/**
+ LABORATORIO DE PROGRAMAÇÃO 2 - LAB 1
+ SOFIA ROCHA CAVALCANTI - 119210421
+ **/
+
+public class hello {
+	public static void main( String[] args) {
+	System.out.println("Hello Dirlididi!");
+
+}
+}
